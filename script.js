f9818a51f3c14ea230417095cc16b588d707dffa
EduardPinte <eduardpinte@gmail.com>
2025-03-27 16:46:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function setup() {
+    createCanvas(400, 400);
+
+}
+
+function draw() {
+    background("#eeeeee");
+    fill("orange");
+    square(110,240,155);
+    fill("grey");
+    quad(140, 395, 180, 395, 180, 310, 140, 310);
+    quad(250, 350, 200, 350, 200, 310, 250, 310);
+    fill("red");
+    triangle(90, 240, 285, 240, 183, 150);
+    fill("green");
+    fill("brown");
+    quad(200, 200, 220, 200, 220, 150, 200, 150);
+    fill("blue");
+    circle(185, 180, 25);
+    line(185,192, 185,168);
+    line(173,180,197,180);
+    line(225,310,225,350);
+    fill("yellow");
+    circle(80, 80, 60);
+    fill("green");
+    triangle(370, 370, 325, 370, 340, 220);
+    fill("brown");
+    square(340,370,15);
+
+
+}
+
